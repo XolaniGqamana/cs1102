1630f8302dd6f88025c4cd491c0eefa4bfbf8e2a
“XolaniGqamana” <xolanig@gmail.com>
2022-03-27 04:59:37 +0000

    Added a template filr.

---
+++ javatempl.java
@@ -0,0 +1,20 @@
+
+/**
+ * A template to use for all Java programs
+ * (Optional-package-declaration)
+ * (Optional-imports)
+ */
+public class programName{
+    /**
+     * (Optional-variable-declarations-and-subroutines)
+     */
+
+    public static void main(String[] args){
+        (statements)
+    
+    }
+    /**
+     * (Optional-variable-dclarations-and-subroutines)
+     */
+
+}

4d1abb867c941ff2146ac6df15cd929608bda98e
“XolaniGqamana” <xolanig@gmail.com>
2022-03-27 05:12:15 +0000

    Chapter 2 Interest Rate

---
+++ Chap2Interest.java
@@ -0,0 +1,21 @@
+
+/**
+ * A template to use for all Java programs
+ * (Optional-package-declaration)
+ * (Optional-imports)
+ */
+public class calculateInt{
+    /**
+     * (Optional-variable-declarations-and-subroutines)
+     */
+
+    public static void main(String[] args){
+        double principal        //The value of the investment
+        double annualInterest   //The annual interest rate
+    
+    }
+    /**
+     * (Optional-variable-dclarations-and-subroutines)
+     */
+
+}

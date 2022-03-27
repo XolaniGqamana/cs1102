5f4bb9f8aedc8022a289e79a0ab5b64652a9bee5
“XolaniGqamana” <xolanig@gmail.com>
2022-03-27 05:35:51 +0000

    New class file added and .java file modified.

--- Chap2Interest.java
+++ Chap2Interest.java
@@ -1,9 +1,13 @@
 
 /**
- * A template to use for all Java programs
- * (Optional-package-declaration)
- * (Optional-imports)
+ * This class implements a program that will
+ * compute the amount of interest that is earned 
+ * on R2000, invested at an annual interest rate 
+ * of 0.027 for one year, the interest and the 
+ * value of the investment after one year are 
+ * printed to standard output.
  */
+
 public class Chap2Interest{
     /**
      * (Optional-variable-declarations-and-subroutines)

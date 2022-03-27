ea0574799f9d3b69896eaa151d1d7554750d7a3b
“XolaniGqamana” <xolanig@gmail.com>
2022-03-27 09:15:04 +0000

    Added data types files.

---
+++ DataTypes.java
@@ -0,0 +1,28 @@
+
+/**
+ * A template to use for all Java programs
+ * (Optional-package-declaration)
+ * (Optional-imports)
+ */
+public class DataTypes{
+    /**
+     * (Optional-variable-declarations-and-subroutines)
+     */
+
+    public static void main(String[] args){
+        int a;
+        String userName;
+        double b, c;
+        boolean isFinished;
+
+        userName = " Xolani"; 
+        System.out.println("");
+        System.out.println(userName);
+        System.out.println("");
+    
+    }
+    /**
+     * (Optional-variable-dclarations-and-subroutines)
+     */
+
+}

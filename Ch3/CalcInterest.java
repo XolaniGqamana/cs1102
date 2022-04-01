60c8671510439a87a139b8eed8a4617a3c957608
“XolaniGqamana” <xolanig@gmail.com>
2022-04-01 17:47:05 +0000

    Chapter 3

---
+++ Ch3/CalcInterest.java
@@ -0,0 +1,11 @@
+
+public class CalcInterest {
+
+public static void main(String[] args) {
+    {
+    
+    }
+
+    }//end of main()
+
+}//end of class
---
+++ Ch3/javatemp.java
@@ -0,0 +1,11 @@
+
+public class ProgramName {
+
+public static void main(String[] args) {
+    {
+    
+    }
+
+    }//end of main()
+
+}//end of class
